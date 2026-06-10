36d3e548b72816dd7a747cd60ba6641476957b14
Munnawar23 <munawwarh48@gmail.com>
2026-06-10 22:39:22 +0530

    feat: implement HomeScreen with pet interactions, state decay logic, and background assets

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -138,6 +138,7 @@ const styles = StyleSheet.create({
   petContainer: {
     justifyContent: 'center',
     alignItems: 'center',
+    marginTop: verticalScale(80),
   },
   petAnimation: {
     width: scale(250),
